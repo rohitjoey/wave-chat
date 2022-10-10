170add2604d08e2e72cbc970a879a2fc6a0b774f
Alex Gurr <me@alexgurr.com>
2022-10-10 16:26:52 +1100

    feat: add prettier to the project

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -17,6 +17,8 @@ module.exports = {
     'airbnb/hooks',
     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+		'prettier', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+		'plugin:prettier/recommended'
   ],
   plugins: ['import'],
   rules: {
